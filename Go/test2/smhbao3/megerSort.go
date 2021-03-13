dc14e992f02c70a9097bba04ef7072cee8f3d86a
LoveJuzi <mylovejuzi@icloud.com>
2021-03-13 20:36:20 +0800

    vim使用技巧

---
+++ Go/test2/smhbao3/megerSort.go
@@ -0,0 +1,8 @@
+package smhbao3
+
+import "fmt"
+
+func MergerSort() {
+	t := A{a: 10, B: 300}
+	fmt.Println(t)
+}
---
+++ Go/test2/smhbao3/quickSort.go
@@ -0,0 +1,10 @@
+package smhbao3
+
+type A struct {
+	a int
+	B int
+}
+
+func Quicksort() {
+
+}
