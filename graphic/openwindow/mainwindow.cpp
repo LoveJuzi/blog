989a8951668ccaf991217192034d2e166c8614b2
LoveJuzi <jike080401@163.com>
2021-06-20 17:44:25 +0800

    open window

--- graphic/openwindow/mainwindow.cpp
+++ graphic/openwindow/mainwindow.cpp
@@ -2,6 +2,7 @@
 
 MainWindow::MainWindow()
 {
+    resize(QSize(800, 600));
 }
 
 void MainWindow::initialize()
@@ -10,6 +11,40 @@ void MainWindow::initialize()
 
 void MainWindow::render()
 {
+    glViewport(0, 0, 800, 600);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *)
+{
+
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *)
+{
+
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *)
+{
+
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *)
+{
+
+}
+
+void MainWindow::mouseDoubleClickEvent(QMouseEvent *)
+{
+
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *)
+{
+
 }
 
 
--- graphic/openwindow/mainwindow.h
+++ graphic/openwindow/mainwindow.h
@@ -12,6 +12,14 @@ public:
 
     void initialize() override;
     void render() override;
+
+protected:
+    void keyPressEvent(QKeyEvent *) override;
+    void keyReleaseEvent(QKeyEvent *) override;
+    void mousePressEvent(QMouseEvent *) override;
+    void mouseReleaseEvent(QMouseEvent *) override;
+    void mouseDoubleClickEvent(QMouseEvent *) override;
+    void mouseMoveEvent(QMouseEvent *) override;
 };
 
 #endif // MAINWINDOW_H
