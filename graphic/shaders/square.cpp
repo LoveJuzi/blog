75b070aae1fa0395f337877d3b5f6f1b581dd8dd
LoveJuzi <mylovejuzi@icloud.com>
2021-06-22 23:30:34 +0800

    shader

--- graphic/shaders/square.cpp
+++ graphic/shaders/square.cpp
@@ -7,10 +7,11 @@ Square::Square(QOpenGLFunctions_3_3_Core* glcore)
 }
 
 static float vertices[] = {
-     0.5f,  0.5f, 0.0f,  // 右上角
-     0.5f, -0.5f, 0.0f,  // 右下角
-    -0.5f, -0.5f, 0.0f,  // 左下角
-    -0.5f,  0.5f, 0.0f   // 左上角
+    // 位置               // 颜色
+     0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+    -0.5f,  0.5f, 0.0f,  0.8f, 0.5f, 0.2f,
 };
 
 static GLuint indices[] = {
@@ -32,8 +33,10 @@ int Square::init()
 
     _glcore->glBindBuffer(GL_ARRAY_BUFFER, VBO);
     _glcore->glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    _glcore->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (GLvoid*)0);
+    _glcore->glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*)0);
     _glcore->glEnableVertexAttribArray(0);
+    _glcore->glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (GLvoid*)(3 * sizeof(float)));
+    _glcore->glEnableVertexAttribArray(1);
 
     _glcore->glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     _glcore->glBufferData(GL_ELEMENT_ARRAY_BUFFER,  sizeof(indices), indices, GL_STATIC_DRAW);
