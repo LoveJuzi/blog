1c70f6e4b7c02a7217f87802b847d45fecaadb25
LoveJuzi <mylovejuzi@icloud.com>
2021-10-30 13:45:27 +0800

    utDefer.h

--- project/01_first_window/main.cpp
+++ project/01_first_window/main.cpp
@@ -4,12 +4,37 @@
 
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+
+#include "common/utDefer.h"
+
 int main() {
     // init GLFW window
     glfwInit();
+    utDefer(glfwTerminate());
+
+    // OpenGL version 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // use OpenGL core mode
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        return -1;
+    }
+
+    // Init GLEW
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    glViewPort(0, 0, 800, 600);
 
     std::cout << "Hello world" << std::endl;
     return 0;
