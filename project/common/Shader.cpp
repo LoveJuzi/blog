31439290931d70029b642fdd68bd45355551a978
LoveJuzi <mylovejuzi@icloud.com>
2021-11-01 20:36:08 +0800

    update

--- project/common/Shader.cpp
+++ project/common/Shader.cpp
@@ -1,5 +1,7 @@
 #include "Shader.h"
 
+#include <glm/gtc/type_ptr.hpp>
+
 #include "utDefer.h"
 
 Shader::Shader(const GLchar* vertexPath, const GLchar* fragmentPath) {
@@ -101,4 +103,8 @@ void Shader::setInt(const std::string& name, int value) const {
 
 void Shader::setFloat(const std::string& name, float value) const {
     glUniform1f(glGetUniformLocation(_id, name.c_str()), value);
+}
+
+void Shader::setMat4(const std::string& name, const glm::mat4& value) const {
+    glUniformMatrix4fv(glGetUniformLocation(_id, name.c_str()), 1, GL_FALSE, glm::value_ptr(value));
 }
--- project/common/Shader.h
+++ project/common/Shader.h
@@ -2,6 +2,7 @@
 #define SHADER_H
 
 #include <GL/glew.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -20,6 +21,7 @@ public:
     void setBool(const std::string& name, bool value) const;
     void setInt(const std::string& name, int value) const;
     void setFloat(const std::string& name, float value) const;
+    void setMat4(const std::string& name, const glm::mat4& value) const;
 
 private:
     unsigned int _id;
