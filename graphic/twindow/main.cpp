19270389cae3af0d9c1285229676e914edf07bb6
LoveJuzi <mylovejuzi@icloud.com>
2021-11-04 21:02:28 +0800

    qt template

---
+++ graphic/twindow/main.cpp
@@ -0,0 +1,13 @@
+#include <QGuiApplication>
+
+#include "twindow.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication a(argc, argv);
+
+    TWindow w;
+    w.show();
+
+    return a.exec();
+}
---
+++ graphic/twindow/twindow.h
@@ -0,0 +1,24 @@
+#ifndef TWINDOW_H
+#define TWINDOW_H
+
+#include <QOpenGLWindow>
+#include <QOpenGLFunctions_3_3_Core>
+
+class TWindow : public QOpenGLWindow, protected QOpenGLFunctions_3_3_Core {
+public:
+    TWindow();
+    ~TWindow() override;
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+
+private:
+    GLuint VAO;           // VAO 对象
+    GLuint VBO;           // VBO 对象
+    GLuint EBO;           // EBO 对象
+    GLuint shaderProgram; // 着色器程序
+};
+
+#endif // TWINDOW_H
