9699908f3419ceeb800e109e6ff9390362c9c849
LoveJuzi <mylovejuzi@icloud.com>
2021-12-04 20:54:48 +0800

    update

--- project/utils/Camera.h
+++ project/utils/Camera.h
@@ -16,7 +16,7 @@ enum CameraMovement {
 // Default camera values
 const float YAW         = -90.0f;
 const float PITCH       =   0.0f;
-const float SPEED       =   0.5f;
+const float SPEED       =   1.0f;
 const float SENSITIVITY =   0.001f;
 //const float SENSITIVITY =   2.5f;
 const float ZOOM        =  45.0f;
