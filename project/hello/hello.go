0a3dfdc3fcfcdf345d051b41fd89d38a574495c6
LoveJuzi <mylovejuzi@icloud.com>
2021-12-17 22:00:45 +0800

    go 的信号

--- project/hello/hello.go
+++ project/hello/hello.go
@@ -2,41 +2,5 @@ package main
 
 import "fmt"
 
-func natureNumber() (<-chan int, func()) {
-	ch := make(chan int)
-	exitCh := make(chan struct{}, 1)
-	exitCh2 := make(chan struct{}, 1)
-
-	go func() {
-		N := 0
-	LOOP:
-		for {
-			select {
-			case <-exitCh:
-				break LOOP
-
-			case ch <- N:
-				N++
-			}
-		}
-		exitCh2 <- struct{}{}
-	}()
-
-	exitFunc := func() {
-		exitCh <- struct{}{}
-		<-exitCh2
-	}
-
-	return ch, exitFunc
-}
-
 func main() {
-	N, exitN := natureNumber()
-	defer exitN()
-
-	fmt.Println(<-N)
-	fmt.Println(<-N)
-	fmt.Println(<-N)
-	fmt.Println(<-N)
-	fmt.Println(<-N)
 }
